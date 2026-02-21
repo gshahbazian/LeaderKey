e0973c5d0805bb836c2ecd79fd40d415432d7dcf
gshahbazian <gabeshahbazian@gmail.com>
2026-02-21 01:47:36 -0800

    need signing with team

--- Leader Key/AppDelegate.swift
+++ Leader Key/AppDelegate.swift
@@ -13,7 +13,6 @@ class AppDelegate: NSObject, NSApplicationDelegate, UNUserNotificationCenterDele
   var state: UserState!
 
   func applicationDidFinishLaunching(_: Notification) {
-
     guard ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] != "1" else { return }
     guard !isRunningTests() else { return }
 
--- bin/release-build.sh
+++ bin/release-build.sh
@@ -7,6 +7,4 @@ BUILD_DIR="$PROJECT_DIR/.build"
 xcodebuild -scheme "Leader Key" -configuration Release build \
   -skipPackagePluginValidation \
   -derivedDataPath "$BUILD_DIR" \
-  CODE_SIGN_IDENTITY="-" \
-  CODE_SIGNING_REQUIRED=NO \
-  CODE_SIGNING_ALLOWED=NO
+  -allowProvisioningUpdates
